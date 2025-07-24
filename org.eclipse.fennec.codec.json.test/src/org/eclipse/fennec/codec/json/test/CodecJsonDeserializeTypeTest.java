97c9f5c2020a1a369101f3883fcc5d6efd1a895a
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2025-07-24 13:52:48 +0200

    added test-data in runproperties for test.bndrun
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- org.eclipse.fennec.codec.json.test/src/org/eclipse/fennec/codec/json/test/CodecJsonDeserializeTypeTest.java
+++ org.eclipse.fennec.codec.json.test/src/org/eclipse/fennec/codec/json/test/CodecJsonDeserializeTypeTest.java
@@ -20,13 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
@@ -36,12 +30,10 @@ import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.fennec.codec.configurator.CodecFactoryConfigurator;
 import org.eclipse.fennec.codec.configurator.CodecModuleConfigurator;
 import org.eclipse.fennec.codec.configurator.ObjectMapperConfigurator;
 import org.eclipse.fennec.codec.options.CodecModelInfoOptions;
-import org.eclipse.fennec.codec.options.CodecModuleOptions;
 import org.eclipse.fennec.codec.options.CodecResourceOptions;
 import org.eclipse.fennec.codec.test.helper.CodecTestHelper;
 import org.gecko.codec.demo.model.person.Child;
@@ -53,14 +45,12 @@ import org.gecko.emf.osgi.annotation.require.RequireEMF;
 import org.gecko.emf.osgi.constants.EMFNamespaces;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.osgi.framework.BundleContext;
 import org.osgi.test.common.annotation.InjectBundleContext;
 import org.osgi.test.common.annotation.InjectService;
 import org.osgi.test.common.annotation.Property;
-import org.osgi.test.common.annotation.config.WithConfiguration;
 import org.osgi.test.common.annotation.config.WithFactoryConfiguration;
 import org.osgi.test.common.service.ServiceAware;
 import org.osgi.test.junit5.cm.ConfigurationExtension;
@@ -157,40 +147,18 @@ public class CodecJsonDeserializeTypeTest extends JsonTestSetting{
 		assertNotNull(p);
 	}
 
-//	Config for the model
-//	Inject here ResourceSet with condition for both codec and my test model
-//	I do not need to load the ecore anymore
-//	(in the json file we need the usual type uri)
-
 	@Test	
 	public void testDeserializationDifferentTypeKeysFeatureLevelDynamicModel(@InjectService(filter = "(" + EMFNamespaces.EMF_CONFIGURATOR_NAME + "=CodecJson)")
 	ServiceAware<ResourceSet> rsAware, @InjectService(filter = "(" + EMFNamespaces.EMF_MODEL_NSURI + "=http://example.de/type/1.0)") ServiceAware<EPackage> ePackageAware ) throws IOException {
 
-		// load ecore
-//		Resource ecoreResource = resourceSet.createResource(URI.createURI(ctx.getBundle().getEntry("test-data/type-as-feature.ecore").toString()));
-//		ecoreResource.load(null);
+
 		EPackage epackage = ePackageAware.getService();
-		EClass child1 = (EClass) epackage.getEClassifier("Child");
-		EClass child2 = (EClass) epackage.getEClassifier("Child2");
+	
 		EClass testClass = (EClass) epackage.getEClassifier("TestObject");
-//
-//		URI uri1 = EcoreUtil.getURI(child1);
-//		URI uri2 = EcoreUtil.getURI(child2);
-//		
-//		URI uri3 = URI.createURI(System.getProperty("basePath") + "test-data/type-as-feature.ecore#//TestObject");
-//		
-//		String t1 = "\"type\": \"http://example.de/type/1.0#//Child\",";
-//		String t2 = "\"kind\": \"http://example.de/type/1.0#//Child2\"";
-//		
-//		String newT1 = "\"type\": \"" + uri1.toString() + "\",";
-//		String newT2 = "\"kind\": \"" + uri2.toString() + "\"";
-//		
-//		substituteType(t1, newT1);
-//		substituteType(t2, newT2);
-//		
+
 
 		// load dynamic eobjects from json with classifier from ecore
-		Resource resource = resourceSet.createResource(URI.createURI("test-data/type-different-keys.json"));
+		Resource resource = resourceSet.createResource(URI.createURI(System.getProperty("test-data") + "type-different-keys.json"));
 
 		Map<String, Object> options = new HashMap<>();
 		options.put(CodecResourceOptions.CODEC_ROOT_OBJECT, testClass);
@@ -224,23 +192,9 @@ public class CodecJsonDeserializeTypeTest extends JsonTestSetting{
 		assertThat(ref2Feature).isNotNull();
 		assertThat(((EObject)loadClass.eGet(ref2Feature)).eClass().getName()).isEqualTo("Child2");	
 		
-//		substituteType(newT1, t1);
-//		substituteType(newT2, t2);
 	}
 
-	private void substituteType(String oldValue, String newValue) throws IOException {
-		List<String> fileContent = new ArrayList<String>(Files.readAllLines(Path.of("test-data/type-different-keys.json"), StandardCharsets.UTF_8));
-
-		for (int i = 0; i < fileContent.size(); i++) {
-			if (fileContent.get(i).contains(oldValue)) {
-				fileContent.set(i, newValue);
-				break;
-			}
-		}
-
-		Files.write(Path.of("test-data/temp.json"), fileContent, StandardCharsets.UTF_8);
-		Files.copy(Path.of("test-data/temp.json"), Path.of("test-data/type-different-keys.json"), StandardCopyOption.REPLACE_EXISTING);
-	}
+	
 
 	@Test
 	public void testDeserializationDifferentTypeKeysFeatureLevel() throws IOException {
@@ -277,67 +231,4 @@ public class CodecJsonDeserializeTypeTest extends JsonTestSetting{
 		assertNotNull(testObj.getRef2());
 		assertTrue(testObj.getRef2() instanceof Child2);
 	}
-	
-//	@Test
-//	public void testDeserializeTypeRootObjMetadataNO() throws IOException {
-//
-//		Resource resource = resourceSet.createResource(URI.createURI(personFileName));
-//
-//		org.eclipse.fennec.codec.test.models.metadata.BusinessPerson person = org.eclipse.fennec.codec.test.models.metadata.MetadataFactory.eINSTANCE.createBusinessPerson();
-//		person.setKind("BusinessPerson");
-//		
-//		resource.getContents().add(person);
-//		Map<String, Object> options = new HashMap<>();
-//		Map<String, Object> classOptions = new HashMap<>();
-//		classOptions.put(CodecModelInfoOptions.CODEC_TYPE_KEY, "kind");
-//		classOptions.put(CodecModelInfoOptions.CODEC_TYPE_STRATEGY, "URI");
-//		classOptions.put(CodecModelInfoOptions.CODEC_TYPE_MAP, Map.of("BusinessPerson", "http://www.eclipse.org/fennec/codec/testmetadata#//BusinessPerson"));
-//		options.put(CodecResourceOptions.CODEC_OPTIONS, Map.of(org.eclipse.fennec.codec.test.models.metadata.MetadataPackage.Literals.BUSINESS_PERSON, classOptions, 
-//				org.eclipse.fennec.codec.test.models.metadata.MetadataPackage.Literals.PERSON, classOptions));
-//		options.put(CodecModuleOptions.CODEC_MODULE_SERIALIZE_TYPE, true);
-//		options.put(CodecModuleOptions.CODEC_MODULE_USE_NAMES_FROM_EXTENDED_METADATA, false);
-//		resource.save(options);
-//		
-//		resource.unload();
-//		resourceSet.getResources().clear();
-//		
-//		Resource loadRes = resourceSet.createResource(URI.createURI(personFileName));
-//		options.put(CodecResourceOptions.CODEC_ROOT_OBJECT, org.eclipse.fennec.codec.test.models.metadata.MetadataPackage.Literals.PERSON);
-//		loadRes.load(options);
-//		
-//		assertFalse(loadRes.getContents().isEmpty());
-//		assertTrue(loadRes.getContents().get(0) instanceof org.eclipse.fennec.codec.test.models.metadata.BusinessPerson);
-//	}
-	
-//	@Test
-//	@Disabled("We have to decide if we really want to support this!")
-//	public void testDeserializeTypeRootObjMetadataYES() throws IOException {
-//
-//		Resource resource = resourceSet.createResource(URI.createURI(personFileName));
-//
-//		org.eclipse.fennec.codec.test.models.metadata.BusinessPerson person = org.eclipse.fennec.codec.test.models.metadata.MetadataFactory.eINSTANCE.createBusinessPerson();
-//		person.setKind("BusinessPerson");
-//		
-//		resource.getContents().add(person);
-//		Map<String, Object> options = new HashMap<>();
-//		Map<String, Object> classOptions = new HashMap<>();
-//		classOptions.put(CodecModelInfoOptions.CODEC_TYPE_KEY, "kind");
-//		classOptions.put(CodecModelInfoOptions.CODEC_TYPE_STRATEGY, "URI");
-//		classOptions.put(CodecModelInfoOptions.CODEC_TYPE_MAP, Map.of("BusinessPerson", "http://www.eclipse.org/fennec/codec/testmetadata#//BusinessPerson"));
-//		options.put(CodecResourceOptions.CODEC_OPTIONS, Map.of(org.eclipse.fennec.codec.test.models.metadata.MetadataPackage.Literals.BUSINESS_PERSON, classOptions, 
-//				org.eclipse.fennec.codec.test.models.metadata.MetadataPackage.Literals.PERSON, classOptions));
-//		options.put(CodecModuleOptions.CODEC_MODULE_SERIALIZE_TYPE, true);
-//		options.put(CodecModuleOptions.CODEC_MODULE_USE_NAMES_FROM_EXTENDED_METADATA, true);
-//		resource.save(options);
-//		
-//		resource.unload();
-//		resourceSet.getResources().clear();
-//		
-//		Resource loadRes = resourceSet.createResource(URI.createURI(personFileName));
-//		options.put(CodecResourceOptions.CODEC_ROOT_OBJECT, org.eclipse.fennec.codec.test.models.metadata.MetadataPackage.Literals.PERSON);
-//		loadRes.load(options);
-//		
-//		assertFalse(loadRes.getContents().isEmpty());
-//		assertTrue(loadRes.getContents().get(0) instanceof org.eclipse.fennec.codec.test.models.metadata.BusinessPerson);
-//	}
 }
