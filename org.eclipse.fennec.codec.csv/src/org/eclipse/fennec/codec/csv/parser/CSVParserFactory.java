96a9666684eff91926f2e9b1d24836e1fa01d8b5
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2025-09-22 15:32:07 +0200

    csv parser (start)
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

---
+++ org.eclipse.fennec.codec.csv/src/org/eclipse/fennec/codec/csv/parser/CSVParserFactory.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2012 - 2025 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
+ */
+package org.eclipse.fennec.codec.csv.parser;
+
+import java.io.InputStream;
+
+import org.eclipse.fennec.codec.CodecParserFactory;
+import org.eclipse.fennec.codec.CodecReaderProvider;
+import org.eclipse.fennec.codec.ecowitt.parser.EcoWittParser;
+
+import tools.jackson.core.io.IOContext;
+
+/**
+ * 
+ * @author ilenia
+ * @since Sep 22, 2025
+ */
+public class CSVParserFactory implements CodecParserFactory<InputStream, CSVParser>{
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.eclipse.fennec.codec.CodecParserFactory#createParser(tools.jackson.core.io.IOContext, org.eclipse.fennec.codec.CodecReaderProvider)
+	 */
+	@Override
+	public CSVParser createParser(IOContext context, CodecReaderProvider<InputStream> provider) {
+		return new CSVParser(context, provider);
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.eclipse.fennec.codec.CodecParserFactory#createParser(tools.jackson.core.io.IOContext, java.lang.Object)
+	 */
+	@Override
+	public CSVParser createParser(IOContext context, InputStream input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
