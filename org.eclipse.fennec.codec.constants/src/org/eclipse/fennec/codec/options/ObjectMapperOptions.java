2d471a28b8e9a2553139a0c6227627470734ddb0
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2025-10-01 09:09:15 +0200

    issue #32 added support for changing FormatFeature via options
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- org.eclipse.fennec.codec.constants/src/org/eclipse/fennec/codec/options/ObjectMapperOptions.java
+++ org.eclipse.fennec.codec.constants/src/org/eclipse/fennec/codec/options/ObjectMapperOptions.java
@@ -38,39 +38,67 @@ public interface ObjectMapperOptions {
 	String OBJ_MAPPER_TIME_ZONE = "obj.mapper.time.zone";
 		
 	/** OBJ_MAPPER_SERIALIZATION_FEATURES_WITH 
-	 * to specify a {@link List} of {@link com.fasterxml.jackson.databind.SerializationFeature} 
+	 * to specify a {@link List} of {@link tools.jackson.databind.SerializationFeature} 
 	 * that should be enabled;
 	 * */
 	String OBJ_MAPPER_SERIALIZATION_FEATURES_WITH = "obj.mapper.serialization.features.with";
 	
 	/** OBJ_MAPPER_SERIALIZATION_FEATURES_WITHOUT 
-	 * to specify a {@link List} of {@link com.fasterxml.jackson.databind.SerializationFeature} 
+	 * to specify a {@link List} of {@link tools.jackson.databind.SerializationFeature} 
 	 * that should be disabled;
 	 * */
 	String OBJ_MAPPER_SERIALIZATION_FEATURES_WITHOUT = "obj.mapper.serialization.features.without";
 	
 	/** OBJ_MAPPER_DESERIALIZATION_FEATURES_WITH 
-	 * to specify a {@link List} of {@link com.fasterxml.jackson.databind.DeserializationFeature} 
+	 * to specify a {@link List} of {@link tools.jackson.databind.DeserializationFeature} 
 	 * that should be enabled;
 	 * */
 	String OBJ_MAPPER_DESERIALIZATION_FEATURES_WITH = "obj.mapper.deserialization.features.with";
 	
 	/** OBJ_MAPPER_DESERIALIZATION_FEATURES_WITHOUT 
-	 * to specify a {@link List} of {@link com.fasterxml.jackson.databind.DeserializationFeature} 
+	 * to specify a {@link List} of {@link tools.jackson.databind.DeserializationFeature} 
 	 * that should be disabled;
 	 * */
 	String OBJ_MAPPER_DESERIALIZATION_FEATURES_WITHOUT = "obj.mapper.deserialization.features.without";
 	
 	/** OBJ_MAPPER_FEATURES_WITH 
-	 * to specify a {@link List} of {@link com.fasterxml.jackson.databind.MapperFeature} 
+	 * to specify a {@link List} of {@link tools.jackson.databind.MapperFeature} 
 	 * that should be enabled;
 	 * */
 	String OBJ_MAPPER_FEATURES_WITH = "obj.mapper.features.with";
 	
 	/** OBJ_MAPPER_FEATURES_WITHOUT 
-	 * to specify a {@link List} of {@link com.fasterxml.jackson.databind.MapperFeature} 
+	 * to specify a {@link List} of {@link tools.jackson.databind.MapperFeature} 
 	 * that should be disabled;
 	 * */
 	String OBJ_MAPPER_FEATURES_WITHOUT = "obj.mapper.features.without";
+	
+	/** OBJ_MAPPER_FORMAT_SER_FEATURES_WITH 
+	 * to specify a {@link List} of {@link tools.jackson.core.FormatFeature} 
+	 * that should be enabled;
+	 * For now we only supports {@link tools.jackson.core.json.JsonWriteFeature}
+	 * */
+	String OBJ_MAPPER_FORMAT_SER_FEATURES_WITH = "obj.mapper.format.ser.features.with";
+	
+	/** OBJ_MAPPER_FORMAT_SER_FEATURES_WITHOUT 
+	 * to specify a {@link List} of {@link tools.jackson.core.FormatFeature} 
+	 * that should be disabled;
+	 * For now we only supports {@link tools.jackson.core.json.JsonWriteFeature}
+	 * */
+	String OBJ_MAPPER_FORMAT_SER_FEATURES_WITHOUT = "obj.mapper.format.ser.features.without";
+	
+	/** OBJ_MAPPER_FORMAT_DESER_FEATURES_WITH 
+	 * to specify a {@link List} of {@link tools.jackson.core.FormatFeature} 
+	 * that should be enabled;
+	 * For now we only supports {@link tools.jackson.core.json.JsonReadFeature}
+	 * */
+	String OBJ_MAPPER_FORMAT_DESER_FEATURES_WITH = "obj.mapper.format.deser.features.with";
+	
+	/** OBJ_MAPPER_FORMAT_DESER_FEATURES_WITHOUT 
+	 * to specify a {@link List} of {@link tools.jackson.core.FormatFeature} 
+	 * that should be disabled;
+	 * For now we only supports {@link tools.jackson.core.json.JsonReadFeature}
+	 * */
+	String OBJ_MAPPER_FORMAT_DESER_FEATURES_WITHOUT = "obj.mapper.format.deser.features.without";
 
 }
