cc3d2c4dca8a6ca4d09784395305ad0ab2aa4b6a
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2025-10-02 09:34:59 +0200

    issue #36: added test (still not working)
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

---
+++ org.eclipse.fennec.codec.test/src/org/eclipse/fennec/codec/test/CodecResourceTest.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2012 - 2023 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
+ */
+package org.eclipse.fennec.codec.test;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.fennec.codec.json.resource.CodecJsonResource;
+import org.eclipse.fennec.codec.jsonschema.resource.CodecJsonSchemaResource;
+import org.gecko.emf.osgi.constants.EMFNamespaces;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.osgi.test.common.annotation.InjectService;
+import org.osgi.test.common.annotation.Property;
+import org.osgi.test.common.annotation.config.WithFactoryConfiguration;
+import org.osgi.test.common.service.ServiceAware;
+import org.osgi.test.junit5.cm.ConfigurationExtension;
+import org.osgi.test.junit5.context.BundleContextExtension;
+import org.osgi.test.junit5.service.ServiceExtension;
+
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * See documentation here: 
+ * 	https://github.com/osgi/osgi-test
+ * 	https://github.com/osgi/osgi-test/wiki
+ * Examples: https://github.com/osgi/osgi-test/tree/main/examples
+ */
+@ExtendWith(BundleContextExtension.class)
+@ExtendWith(ServiceExtension.class)
+@ExtendWith(ConfigurationExtension.class)
+@WithFactoryConfiguration(factoryPid = "DefaultCodecFactoryConfigurator", location = "?", name = "test", properties = {
+		@Property(key = "type", value="json")
+})
+@WithFactoryConfiguration(factoryPid = "DefaultObjectMapperConfigurator", location = "?", name = "test", properties = {
+		@Property(key = "type", value="json")
+})
+@WithFactoryConfiguration(factoryPid = "DefaultCodecModuleConfigurator", location = "?", name = "test", properties = {
+		@Property(key = "type", value="json")
+})
+public class CodecResourceTest {
+	
+	@InjectService(cardinality = 0, filter = "(" + EMFNamespaces.EMF_CONFIGURATOR_NAME + "=CodecJson)")
+	ServiceAware<ResourceSet> rsAware;
+	
+	private ResourceSet resourceSet;
+	
+	@BeforeEach()
+	public void beforeEach() throws InterruptedException{
+		resourceSet = rsAware.waitForService(2000l);
+		assertNotNull(resourceSet);
+	}
+
+	@Test
+	public void testCodecResource()  {
+	
+		Resource jsonRes = resourceSet.createResource(URI.createURI("test.json"), "application/json");
+		assertInstanceOf(CodecJsonResource.class, jsonRes);
+		
+		Resource jsonSchemaRes = resourceSet.createResource(URI.createURI("test.json"), "application/schema+json");
+		assertInstanceOf(CodecJsonSchemaResource.class, jsonSchemaRes);
+	}
+	
+	
+}
