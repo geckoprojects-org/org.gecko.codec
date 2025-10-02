e550be1c39d26c242cdf290dd1475b198b9f728d
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2025-10-02 13:07:36 +0200

    adjust date format tests
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- org.eclipse.fennec.codec.test/src/org/eclipse/fennec/codec/test/ObjMapperConfiguratorTest.java
+++ org.eclipse.fennec.codec.test/src/org/eclipse/fennec/codec/test/ObjMapperConfiguratorTest.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
@@ -388,15 +387,8 @@ public class ObjMapperConfiguratorTest {
 		ObjectMapper mapper = mapperConfigurator.getObjMapperBuilderFactory().createObjectMapperBuilder().build();
 		assertNotNull(mapper);
 		
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		DateFormat mapperDF = mapper.serializationConfig().getDateFormat();
-		assertEquals(df.format(Date.from(                     // Convert from modern java.time class to troublesome old legacy class.  DO NOT DO THIS unless you must, to inter operate with old code not yet updated for java.time.
-			    LocalDate.of(1990, 6, 20)                        // `LocalDate` class represents a date-only, without time-of-day and without time zone nor offset-from-UTC. 
-			    .atStartOfDay(                       // Let java.time determine the first moment of the day on that date in that zone. Never assume the day starts at 00:00:00.
-			        ZoneId.of( "Europe/Berlin" )  // Specify time zone using proper name in `continent/region` format, never 3-4 letter pseudo-zones such as “PST”, “CST”, “IST”. 
-			    )                                    // Produce a `ZonedDateTime` object. 
-			    .toInstant()                         // Extract an `Instant` object, a moment always in UTC.
-			)), mapperDF.format(Date.from(                     // Convert from modern java.time class to troublesome old legacy class.  DO NOT DO THIS unless you must, to inter operate with old code not yet updated for java.time.
+		assertEquals("1990-06-20", mapperDF.format(Date.from(                     // Convert from modern java.time class to troublesome old legacy class.  DO NOT DO THIS unless you must, to inter operate with old code not yet updated for java.time.
 				    LocalDate.of(1990, 6, 20)                        // `LocalDate` class represents a date-only, without time-of-day and without time zone nor offset-from-UTC. 
 				    .atStartOfDay(                       // Let java.time determine the first moment of the day on that date in that zone. Never assume the day starts at 00:00:00.
 				        ZoneId.of( "Europe/Berlin" )  // Specify time zone using proper name in `continent/region` format, never 3-4 letter pseudo-zones such as “PST”, “CST”, “IST”. 
