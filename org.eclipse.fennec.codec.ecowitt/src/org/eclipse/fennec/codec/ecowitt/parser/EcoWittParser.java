b4b3e941c3908d2e8dfbd541e09e92dbb742767b
Guido Grune <g.grune@datainmotion.com>
2025-11-14 10:13:04 +0100

    fix license header issues
    Signed-off-by: Guido Grune <g.grune@datainmotion.com>

--- org.eclipse.fennec.codec.ecowitt/src/org/eclipse/fennec/codec/ecowitt/parser/EcoWittParser.java
+++ org.eclipse.fennec.codec.ecowitt/src/org/eclipse/fennec/codec/ecowitt/parser/EcoWittParser.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012 - 2024 Data In Motion and others.
+ * Copyright (c) 2012 - 2025 Data In Motion and others.
  * All rights reserved. 
  * 
  * This program and the accompanying materials are made
